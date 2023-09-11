46960ac734c3d3172274048ba5433a9c3d899aee
spread1337 <samarplayz1337@gmail.com>
2023-09-11 15:55:52 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+	name='lastfm',
+	version="0.0.1",
+	description="a library for easy access to lastfm data for discord bots",
+	long_description="...",
+	long_description_content_type="text/markdown",
+	author="spread1337",
+	author_email="neon@flarebot.tech",
+	url="https://github.com/spread1337/lastfm.git",
+	packages=['lastfm'],
+	install_requires=['aiohttp','discord','orjson'],
+	python_requires=">=3.6"
+)
